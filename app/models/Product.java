8bc24c8fa76b2e4e49d23566bcd9d9ba81340793
Phillip Verheyden <pverheyden@gmail.com>
2011-10-12 22:04:49 -0500

    Forgot a property in the Product class

--- app/models/Product.java
+++ app/models/Product.java
@@ -11,13 +11,15 @@ public class Product extends Model {
 	public double width;
 	public double height;
 	public Address location;
+	public String url;
 	
 	public Product() { }
-	public Product(String name, double width, double height, Address location) {
+	public Product(String name, double width, double height, Address location, String url) {
 	    this.name = name;
 	    this.width = width;
 	    this.height = height;
 	    this.location = location;
+	    this.url = url;
 	}
 	
 
